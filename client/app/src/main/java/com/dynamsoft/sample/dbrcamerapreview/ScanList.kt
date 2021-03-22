2b0b4ed0d34a08d68c20e463898b1ca87bb0fc1e
Will Pringle <wpringle@uoguelph.ca>
2021-03-22 06:35:04 -0400

    Add item name in list

--- client/app/src/main/java/com/dynamsoft/sample/dbrcamerapreview/ScanList.kt
+++ client/app/src/main/java/com/dynamsoft/sample/dbrcamerapreview/ScanList.kt
@@ -6,10 +6,12 @@ import android.widget.ArrayAdapter
 import android.widget.ListView
 import com.android.volley.Request
 import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import org.json.JSONArray
 import org.json.JSONException
+import org.json.JSONObject
 
 
 class ScanList : AppCompatActivity() {
@@ -17,44 +19,47 @@ class ScanList : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan_list)
 
-        val lastScans: MutableList<String> = ArrayList<String>()
-
-        // populate string arraylist
-        lastScans.add("hello")
-        lastScans.add("world")
-        lastScans.add("today")
-
         val listView: ListView = findViewById(R.id.lastscanslistview)
-
-
-        println("volleyGet~~~~~~~~~~~~~~~~~~~~~~ start")
-        val url = "http://173.34.40.62:5000/getLastScans"
         val jsonResponses: MutableList<String> = ArrayList()
-        val requestQueue = Volley.newRequestQueue(this)
-        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null, Response.Listener { response ->
-            println("inside callback function")
+        val url = "http://173.34.40.62:5000/"
+        val callback = { response : JSONArray ->
             try {
-                val jsonArray = response.getJSONArray("data")
+                val jsonArray = response
+
                 for (i in 0 until jsonArray.length()) {
                     val jsonObject = jsonArray.getJSONObject(i)
                     val barcode_ID = jsonObject.getString("BARCODE_ID")
-                    jsonResponses.add(barcode_ID)
+                    // jsonResponses.add(barcode_ID)
+                    volleyGetJsonObject(url + "getinfo?serial=" + barcode_ID) { itemResponse : JSONObject ->
+                        jsonResponses.add(
+                                "Name: " + itemResponse.getString("PRODUCT_TITLE") + "\n" + "Barcode: " + itemResponse.getString("SERIAL_NUMBER")
+                        )
+                        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, jsonResponses)
+                        listView.setAdapter(adapter)
+                    }
                 }
-
-                // count the number of json repsonses
-
-                val adapter = ArrayAdapter<String>(
-                        this, android.R.layout.simple_list_item_1, jsonResponses
-                )
-
-                listView.setAdapter(adapter)
 
             } catch (e: JSONException) {
                 e.printStackTrace()
             }
+        }
+
+        volleyGetJsonArray(url + "getLastScans", callback)
+    }
+
+    private fun volleyGetJsonArray(endPoint : String, callback : (response : JSONArray) -> Unit) {
+        val requestQueue = Volley.newRequestQueue(this)
+        val jsonObjectRequest = JsonArrayRequest(Request.Method.GET, endPoint, null, Response.Listener { response ->
+            callback(response)
         }, Response.ErrorListener { error -> error.printStackTrace() })
         requestQueue.add(jsonObjectRequest)
+    }
 
-
+    private fun volleyGetJsonObject(endPoint : String, callback : (response : JSONObject) -> Unit) {
+        val requestQueue = Volley.newRequestQueue(this)
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, endPoint, null, Response.Listener { response ->
+            callback(response)
+        }, Response.ErrorListener { error -> error.printStackTrace() })
+        requestQueue.add(jsonObjectRequest)
     }
 }
